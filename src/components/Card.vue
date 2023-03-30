c264db1d256d85db6918175545fb0328935a346a
daniloeberli <danilo.eberli@gmail.com>
2023-03-30 17:28:33 +0200

    fix: milestone 3, added default thumbnail

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,28 +3,28 @@ import CountryFlag from 'vue-country-flag-next';
 
 export default {
     name: 'Card',
-    components:{
+    components: {
         CountryFlag
     },
     props: {
         info: Object
     },
-    computed:{
-        getVote(){
+    computed: {
+        getVote() {
             // trasforma il voto dalla scala decimale a quella a 5 stelle
             return Math.ceil(this.info.vote_average / 2)
         },
-        getTitle(){
+        getTitle() {
             // implementazione logica a seconda che sia film o serie tv
             return this.info.title ? this.info.title : this.info.name
         },
-        getOriginalTitle(){
+        getOriginalTitle() {
             // implementazione logica a seconda che sia film o serie tv
             return this.info.original_title ? this.info.original_title : this.info.original_name
         },
-        getLanguage(){
+        getLanguage() {
             //switch per i casi limite delle lingue
-            switch(this.info.original_language){
+            switch (this.info.original_language) {
                 case "en":
                     return 'gb';
                 case 'he':
@@ -40,14 +40,19 @@ export default {
                 case 'ja':
                     return 'jp';
                 case 'sr':
-                    return 'rs'        
+                    return 'rs'
                 default:
                     return this.info.original_language
 
             }
         },
-        getImage(){
-            return `https://image.tmdb.org/t/p/w342${this.info.backdrop_path}`
+        getImage() {
+            //metodo che ci restituisce la thumbnail, se non esiste mostra img placeholder
+            if (this.info.backdrop_path === null) {
+                return `https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`
+            } else {
+                return `https://image.tmdb.org/t/p/w342${this.info.backdrop_path}`
+            }
         }
     }
 }
@@ -59,9 +64,9 @@ export default {
             <img class="img-fluid" :src=getImage :alt=getOriginalTitle>
             <li><span>Titolo:</span> {{ getTitle }}</li>
             <li><span>Titolo originale:</span>{{ getOriginalTitle }}</li>
-            <li><country-flag :country='getLanguage' size='small'/></li>
+            <li><country-flag :country='getLanguage' size='small' /></li>
             <li><span>Valutazione media:</span>{{ getVote }}</li>
-            <font-awesome-icon icon="fa-solid fa-star" v-for="n in getVote"/>
+            <font-awesome-icon icon="fa-solid fa-star" v-for="n in getVote" />
             <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5 - getVote" />
         </ul>
     </article>
