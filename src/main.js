96fb7604aa65568ef728ed03e21d59ea0a6723de
daniloeberli <danilo.eberli@gmail.com>
2023-03-30 14:20:34 +0200

    script: font awesome, country flag

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import CountryFlag from 'vue-country-flag'
 import './assets/scss/main.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { faStar as fastarsolid } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).mount('#app')
+library.add(faStar, fastarsolid)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
