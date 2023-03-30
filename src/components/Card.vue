a13d06353da5ed4423c168a074431db023fd1113
daniloeberli <danilo.eberli@gmail.com>
2023-03-30 16:37:52 +0200

    feat: milestone 2 completed

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,10 @@
 <script>
+import CountryFlag from 'vue-country-flag-next';
 
 export default {
     name: 'Card',
     components:{
-        
+        CountryFlag
     },
     props: {
         info: Object
@@ -17,6 +18,27 @@ export default {
         },
         getOriginalTitle(){
             return this.info.original_title ? this.info.original_title : this.info.original_name
+        },
+        getLanguage(){
+            switch(this.info.original_language){
+                case "en":
+                    return 'gb';
+                case 'he':
+                    return 'il';
+                case 'sv':
+                    return 'se';
+                case 'uk':
+                    return 'ua';
+                case 'zh':
+                    return 'cn';
+                case 'ko':
+                    return 'kr';
+                case 'ja':
+                    return 'jp'    
+                default:
+                    return this.info.original_language
+
+            }
         }
     }
 }
@@ -26,7 +48,7 @@ export default {
         <ul>
             <li><span>Titolo:</span> {{ getTitle }}</li>
             <li><span>Titolo originale:</span>{{ getOriginalTitle }}</li>
-            <li><span>Lingua:</span>{{ info.original_language }}</li>
+            <li><country-flag :country='getLanguage' size='small'/></li>
             <li><span>Valutazione media:</span>{{ getVote }}</li>
             <font-awesome-icon icon="fa-solid fa-star" v-for="n in getVote"/>
             <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5 - getVote" />
